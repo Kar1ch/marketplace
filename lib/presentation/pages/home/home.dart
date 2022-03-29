b3965d789807cc795c5a355cf537b6074175e2b7
Hitfot <hitfot@mail.ru>
2022-03-29 15:28:22 +0300

    Commit 3

--- lib/presentation/pages/home/home.dart
+++ lib/presentation/pages/home/home.dart
@@ -1,61 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:marketplace/presentation/colors.dart';
+import 'package:marketplace/presentation/pages/home/widgets/cards.dart';
 
-class MainPage extends StatefulWidget{
-  const MainPage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget{
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  _MainPageState createState() => _MainPageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MainPageState extends State<MainPage>{
-  int _selectedIndex = 0;
-   var pages = [];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+class _HomePageState extends State<HomePage>{
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: const Text('Marketplace'),
-        actions: <Widget>[
-          IconButton(onPressed: (){}, icon: const Icon(Icons.search, color: Colors.white,)),
-        ],
-      ),
-      body: pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Главная',
-              backgroundColor: Colors.green,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.apps_outlined),
-            label: 'Каталог',
-            backgroundColor: Colors.green,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
-            label: 'Корзина',
-            backgroundColor: Colors.green,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_circle),
-            label: 'Профиль',
-            backgroundColor: Colors.green,
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.black,
-        onTap: _onItemTapped,
-      ),
+
     );
   }
 }
--- lib/presentation/pages/home/widgets/cards.dart
+++ lib/presentation/pages/home/widgets/cards.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
 
-class Card1 extends Card{
-  const Card1({Key? key}) : super(key: key);
+class Card1 extends StatelessWidget{
+  const Card1(
+      {Key? key, required this.imagePath, required this.onClick, required this.label})
+      : super(key: key);
 
+  final String label;
+  final String imagePath;
+  final Function onClick;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: TextButton(
+        onPressed: () => onClick,
+        child: Column(
+          children: [
+            Image.asset(imagePath),
+            Text(label),
+          ]
+        )
+      ),
+    );
+  }
 }
