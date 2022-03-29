617bc31ed172262dc7ca2714f8d931db532cd85a
Hitfot <hitfot@mail.ru>
2022-03-29 14:37:30 +0300

    Commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'presentation/pages/home.dart';
 
 void main() {
   runApp(
@@ -7,57 +8,4 @@ void main() {
         home: HomePage(),
       )
   );
-}
-
-class HomePage extends StatefulWidget{
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        title: const Text('Marketplace'),
-        actions: <Widget>[
-          IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.white,)),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text('hitfot'),
-              accountEmail: Text('hitfot@mail.ru'),
-              currentAccountPicture: GestureDetector(
-                child: const CircleAvatar(
-                  backgroundColor: Colors.white,
-                  child: Icon(Icons.person, color: Colors.black, size: 50,),
-                ),
-              ),
-              decoration: const BoxDecoration(
-                color: Colors.green,
-              ),
-            ),
-            InkWell(
-              onTap: (){},
-              child: const ListTile(
-                leading: Icon(Icons.home),
-                title: Text('Title'),
-              )
-            ),
-            InkWell(
-              onTap: (){},
-              child: const ListTile(
-                leading: Icon(Icons.shopping_cart),
-                title: Text('Cart'),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
 }
--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -1,17 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:marketplace/presentation/colors.dart';
 
-class HomePage extends StatelessWidget{
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget{
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>{
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: DarkThemeColors.background02,
-        title: const Text('Text'),
+        backgroundColor: Colors.green,
+        title: const Text('Marketplace'),
+        actions: <Widget>[
+          IconButton(onPressed: (){}, icon: Icon(Icons.search, color: Colors.white,)),
+        ],
       ),
-      backgroundColor: DarkThemeColors.background01,
+      body: Center(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Главная',
+              backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.apps_outlined),
+            label: 'Каталог',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Корзина',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Профиль',
+            backgroundColor: Colors.green,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.black,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
